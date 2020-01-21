b3e0e617a5891f47e3351a955888225f1a69ab1e
David Smith <david.smith@weaveability.com>
2020-01-21 13:40:53 +0000

    apply conditional nav styling

--- src/nav.js
+++ src/nav.js
@@ -1,19 +1,21 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+function getPrevClassName(props) {
+  return "dft__nav__btn dft__nav__btn--prev";
+}
+
+function getNextClassName(props) {
+  return "dft__nav__btn dft__nav__btn--next";
+}
+
 function Nav(props) {
   return (
     <div className="dft__nav">
-      <button
-        className="dft__nav__btn dft__nav__btn--prev"
-        onClick={props.onPrevious}
-      >
+      <button className={getPrevClassName(props)} onClick={props.onPrevious}>
         &#10094;
       </button>
-      <button
-        className="dft__nav__btn dft__nav__btn--next"
-        onClick={props.onNext}
-      >
+      <button className={getNextClassName(props)} onClick={props.onNext}>
         &#10095;
       </button>
     </div>
