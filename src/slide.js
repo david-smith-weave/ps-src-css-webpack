e34430468f83d237b2041be0f00fbd18d98162ff
David Smith <david.smith@weaveability.com>
2020-01-21 13:33:21 +0000

    new slide styles

--- src/slide.js
+++ src/slide.js
@@ -1,22 +1,22 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 
 function Slide(props) {
   return (
-    <article style={props.style}>
+    <article style={props.style} className="dft__slide">
       <img src={props.image} alt={props.title} />
-      <footer>
-        <h2>{props.title}</h2>
+      <footer className="dft__slide__footer">
+        <h2 className="dft__slide__footer__title">{props.title}</h2>
         <div>{props.children}</div>
       </footer>
     </article>
-  )
+  );
 }
 
 Slide.propTypes = {
   image: PropTypes.string.isRequired,
   style: PropTypes.object,
   title: PropTypes.string
-}
+};
 
-export default Slide
+export default Slide;
